8c177763abaad68ea3644c259094efe69f728947
devaanantawijaya <gededeva11@gmail.com>
2025-01-24 16:36:51 +0800

    tambah custom hook activities

---
+++ src/hooks/useActivities.js
@@ -0,0 +1,27 @@
+import { API_KEY, BASE_URL } from "@/helper/endpoint";
+import axios from "axios";
+import { useState } from "react";
+
+const useActivities = () => {
+  const [activities, setActivities] = useState([]);
+  const [loadingActivities, setLoadingActivities] = useState(false);
+
+  const getActivities = async () => {
+    setLoadingActivities(true);
+    try {
+      const res = await axios.get(`${BASE_URL.API}/api/v1/activities`, {
+        headers: {
+          apiKey: API_KEY,
+        },
+      });
+      setActivities(res.data.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoadingActivities(false);
+    }
+  };
+  return { activities, loadingActivities, getActivities };
+};
+
+export default useActivities;
