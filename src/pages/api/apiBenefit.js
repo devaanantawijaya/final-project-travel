62ccdb49e34f60b4bde8083ff0f20b147c5e02c8
devaanantawijaya <gededeva11@gmail.com>
2025-01-24 16:19:00 +0800

    update benefit

---
+++ src/pages/api/apiBenefit.js
@@ -0,0 +1,18 @@
+export const apiBenefit = [
+  {
+    title: "Promo Melimpah",
+    type: "promo",
+    description:
+      "Raih promo melimpah dengan diskon hingga 50% untuk berbagai destinasi menarik! Nikmati paket liburan lengkap dan fasilitas eksklusif dengan harga terjangkau hanya di situs kami.",
+    imageUrl:
+      "https://asset-2.tstatic.net/tribunnews/foto/bank/images/daftar-promo-tiket-wisata-spesial-hut-ke-79-ri-ada-tmii-ancol-hingga-taman-safari.jpg",
+  },
+  {
+    title: "Tour Lengkap",
+    type: "tour",
+    description:
+      "Temukan segudang pilihan tour menarik dengan harga spesial! Pilih destinasi impian Anda dan nikmati pengalaman liburan tak terlupakan.",
+    imageUrl:
+      "https://www.gardaoto.com/wp-content/uploads/2023/05/Screenshot-2023-05-05-180124.png",
+  },
+];
