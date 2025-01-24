56f7cf677b1568626226ffbeb61fcc3547091710
devaanantawijaya <gededeva11@gmail.com>
2025-01-24 16:37:42 +0800

    tambah component slide responsive

---
+++ src/components/Slides/Responsive/index.js
@@ -0,0 +1,82 @@
+import React, { useRef } from "react";
+import Slider from "react-slick";
+import { IoIosArrowDropright } from "react-icons/io";
+import { IoIosArrowDropleft } from "react-icons/io";
+import Card from "@/components/Card";
+
+const SlideResponsive = (props) => {
+  const { title, items, extraTitle } = props;
+
+  const sliderRef = useRef(null);
+
+  const settings = {
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    autoplay: true,
+    cssEase: "linear",
+    nextArrow: null,
+    prevArrow: null,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="xl:pt-10 pt-5">
+      <div className="flex justify-between items-center xl:px-20 px-4">
+        <div className="flex items-center pb-5 gap-x-3">
+          <h1 className="font-bold text-2xl">{title}</h1>
+          {extraTitle && <p>{extraTitle}</p>}
+        </div>
+        <div className="flex">
+          <p
+            onClick={() => sliderRef.current.slickPrev()}
+            className="cursor-pointer text-orange-400 hover:text-orange-600 text-2xl"
+          >
+            <IoIosArrowDropleft />
+          </p>
+          <p
+            onClick={() => sliderRef.current.slickNext()}
+            className="cursor-pointer text-orange-400 hover:text-orange-600 text-2xl"
+          >
+            <IoIosArrowDropright />
+          </p>
+        </div>
+      </div>
+      <div className="xl:px-16">
+        <Slider {...settings} ref={sliderRef}>
+          {items.map((item) => (
+            <Card item={item} key={item.id} title={title} />
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default SlideResponsive;
