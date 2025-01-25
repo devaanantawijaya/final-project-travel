646806ef5965f16bf6d9c724db183974b9a98fab
devaanantawijaya <gededeva11@gmail.com>
2025-01-25 17:52:43 +0800

    update Call to Action

--- src/components/CTA/index.js
+++ src/components/CTA/index.js
@@ -1,17 +1,17 @@
 import Button from "../Button";
 
 const CTA = (props) => {
-  const { buttonCTA, wordFirst, hookCTA } = props;
+  const { buttonCTA, wordFirst, hookCTA, sentenceCTA } = props;
 
   return (
-    <div className="absolute inset-0 flex items-center justify-center">
-      <div className="text-center grid grid-cols-1 xl:gap-y-5 gap-y-2 w-72 sm:w-full">
-        <h1 className="font-bold xl:text-5xl text-white xl:gap-3 md:text-4xl text-2xl">
+    <div className="absolute top-1/3 right-0 w-full">
+      <div className="flex-col flex xl:gap-y-5 gap-y-2 w-full justify-center items-center">
+        <h1 className="font-bold xl:text-5xl text-white xl:gap-3 md:text-4xl text-2xl text-center">
           {wordFirst} <span className="text-orange-400">{hookCTA}</span>{" "}
           Sekarang!
         </h1>
-        <p className="font-semibold xl:text-xl text-white xl:block">
-          Klik sekarang untuk mulai dan buat perbedaan di hidup Anda!
+        <p className="font-semibold xl:text-xl text-white text-center w-72 sm:w-[800px]">
+          {sentenceCTA}
         </p>
         <div>
           <Button
