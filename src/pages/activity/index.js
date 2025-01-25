52cdf30a5829b969195a5282ab62eff6cb152e53
devaanantawijaya <gededeva11@gmail.com>
2025-01-25 17:57:33 +0800

    update halaman activity

---
+++ src/pages/activity/index.js
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+const ActivityPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="pt-28">ini halaman activity</div>
+    </div>
+  );
+};
+
+export default ActivityPage;
