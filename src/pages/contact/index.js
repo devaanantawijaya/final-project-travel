3a97a6cb12b4ddfc4290cadda19b2c932d622c29
devaanantawijaya <gededeva11@gmail.com>
2025-01-25 17:57:46 +0800

    tambah halaman contact

---
+++ src/pages/contact/index.js
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="pt-28">ini halaman Contact</div>
+    </div>
+  );
+};
+
+export default ContactPage;
