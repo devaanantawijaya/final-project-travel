97b16cadda27e8b6732f81ea92ef318c5f5b6336
devaanantawijaya <gededeva11@gmail.com>
2025-01-25 17:55:25 +0800

    tambah halaman about

---
+++ src/pages/about/index.js
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="pt-28">ini halaman About</div>
+    </div>
+  );
+};
+
+export default AboutPage;
