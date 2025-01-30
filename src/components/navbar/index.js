e0f2bccb0fe055c846921e6f9c57db2a2208b4b7
devaanantawijaya <gededeva11@gmail.com>
2025-01-30 19:08:23 +0800

    menambahkan foto dan nama user pada navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -2,17 +2,22 @@ import Link from "next/link";
 import Button from "../Button";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import { deleteCookie, getCookie } from "cookies-next";
+import { FaLuggageCart } from "react-icons/fa";
+import { useUser } from "@/context/userContext";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
-  const [token, setToken] = useState(null);
+  const { user } = useUser();
   const router = useRouter();
+  const token = getCookie("JWT_TOKEN");
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    const token = getCookie("JWT_TOKEN");
-    setToken(token);
+    setIsClient(true);
   }, []);
+
+  if (!isClient) return null;
 
   const toLoginPage = () => {
     router.push("/login");
@@ -20,12 +25,6 @@ const Navbar = () => {
 
   const toRegisterPage = () => {
     router.push("/register");
-  };
-
-  const handleLogout = () => {
-    deleteCookie("JWT_TOKEN");
-    setToken(null);
-    router.push("/login");
   };
 
   return (
@@ -68,14 +67,30 @@ const Navbar = () => {
       </div>
       <div className="xl:block hidden">
         {token ? (
-          <Button
-            title="Logout"
-            bg="bg-orange-400 hover:bg-orange-600 text-white"
-            p="px-7 py-1"
-            onClick={handleLogout}
-          />
+          <div className="flex items-center gap-5">
+            {user && user.role === "user" ? (
+              <Link href="/user/cart">
+                <FaLuggageCart className="text-3xl hover:text-orange-500" />
+              </Link>
+            ) : (
+              ""
+            )}
+
+            <Link href="/user">
+              <div className="flex gap-x-3 items-center">
+                <img
+                  src={user?.profilePictureUrl}
+                  alt="User Profile"
+                  className="w-10 h-10 rounded-full"
+                />
+                <h1 className="font-semibold hover:text-orange-400">
+                  {user?.name}
+                </h1>
+              </div>
+            </Link>
+          </div>
         ) : (
-          <div className=" flex gap-0">
+          <div className="flex gap-0">
             <Button
               title="Login"
               text="hover:text-orange-400 text-black"
