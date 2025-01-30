fa125744de0a0ba4f9a80c8dac65247a984221e9
devaanantawijaya <gededeva11@gmail.com>
2025-01-30 19:09:35 +0800

    tambah useContext

---
+++ src/context/userContext.js
@@ -0,0 +1,25 @@
+import { deleteCookie, getCookie } from "cookies-next";
+import { useRouter } from "next/router";
+import { createContext, useContext, useState, useEffect } from "react";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  // const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const savedUser = localStorage.getItem("user");
+      savedUser && setUser(JSON.parse(savedUser));
+    }
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => useContext(UserContext);
