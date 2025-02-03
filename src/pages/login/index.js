d2db6c8fb83fa187a0fbec6ae16713f089d80fe4
devaanantawijaya <gededeva11@gmail.com>
2025-02-04 00:56:11 +0800

    menambahkan pop up success dan failed ketika login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -7,6 +7,7 @@ import { setCookie } from "cookies-next";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
 const LoginPage = () => {
   const { setUser } = useUser();
@@ -27,63 +28,96 @@ const LoginPage = () => {
       const userData = res.data.data;
       localStorage.setItem("user", JSON.stringify(userData));
       setUser(userData);
-      router.push("/");
+      Swal.fire({
+        title: "Login Success",
+        text: res.data.message,
+        icon: "success",
+        confirmButtonText: "OK",
+        timer: 1100,
+        customClass: {
+          confirmButton:
+            "bg-orange-500 hover:bg-orange-400 text-white font-bold py-2 px-4 rounded",
+        },
+      }).then(() => {
+        router.push("/");
+      });
     } catch (error) {
       console.log(error);
+      Swal.fire({
+        title: "Login Failed",
+        text: error.response.data.message,
+        icon: "error",
+        confirmButtonText: "Try Again",
+        timer: 2000,
+        customClass: {
+          confirmButton:
+            "bg-orange-500 hover:bg-orange-400 text-white font-bold py-2 px-4 rounded",
+        },
+      });
     }
   };
 
   return (
     <div>
       <Navbar />
-      <div className="flex justify-center items-center sm:pt-48 pt-40 sm:px-0 px-5">
-        <div className="bg-slate-100 sm:p-10 p-8 w-96 rounded-2xl shadow-2xl">
-          <div className="text-center mb-5">
-            <h1 className="text-4xl font-bold text-orange-400">WELCOME</h1>
-            <p>Login with Email</p>
-          </div>
+      <div className="relative">
+        <div className="w-full xl:aspect-[16/8] overflow-hidden sm:aspect-square aspect-[9/16]">
+          <img
+            src="/images/liburan.jpg"
+            alt="bg-liburan"
+            className="w-full h-full object-cover"
+          />
+        </div>
 
-          {/* Email */}
-          <div className="mb-5">
-            <input
-              type="text"
-              placeholder="Enter Email"
-              className="border-2 w-full text-xl p-2 rounded-lg border-orange-400"
-              onChange={(e) =>
-                setFormData({ ...formData, email: e.target.value })
-              }
-            />
-          </div>
+        <div className="flex justify-center items-center sm:px-0 px-5 absolute xl:top-1/4 xl:right-28 xl:inset-auto inset-0">
+          <div className="bg-slate-100 sm:p-10 p-8 w-96 rounded-2xl shadow-2xl">
+            <div className="text-center mb-5">
+              <h1 className="text-4xl font-bold text-orange-400">WELCOME</h1>
+              <p>Login with Email</p>
+            </div>
 
-          {/* Password */}
-          <div className="mb-5">
-            <input
-              type="text"
-              placeholder="Enter Password"
-              className="border-2 w-full text-xl p-2 rounded-lg border-orange-400"
-              onChange={(e) =>
-                setFormData({ ...formData, password: e.target.value })
-              }
-            />
-          </div>
+            {/* Email */}
+            <div className="mb-5">
+              <input
+                type="text"
+                placeholder="Enter Email"
+                className="border-2 w-full text-xl p-2 rounded-lg border-orange-400"
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+              />
+            </div>
 
-          {/* Button */}
-          <div className="flex justify-center">
-            <Button
-              title="LOGIN"
-              bg="bg-orange-400 hover:bg-orange-600 w-full"
-              text="text-white"
-              p="py-1"
-              onClick={handleLogin}
-            />
-          </div>
+            {/* Password */}
+            <div className="mb-5">
+              <input
+                type="text"
+                placeholder="Enter Password"
+                className="border-2 w-full text-xl p-2 rounded-lg border-orange-400"
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
+              />
+            </div>
 
-          {/* Switch to RegisterPage */}
-          <div className="text-center pt-3">
-            {`Dont't have account?`}{" "}
-            <span className="text-orange-400 hover:text-orange-600 hover:font-semibold">
-              <Link href="/register">Register Now</Link>
-            </span>
+            {/* Button */}
+            <div className="flex justify-center">
+              <Button
+                title="LOGIN"
+                bg="bg-orange-400 hover:bg-orange-600 w-full"
+                text="text-white"
+                p="py-1"
+                onClick={handleLogin}
+              />
+            </div>
+
+            {/* Switch to RegisterPage */}
+            <div className="text-center pt-3">
+              {`Dont't have account?`}{" "}
+              <span className="text-orange-400 hover:text-orange-600 hover:font-semibold">
+                <Link href="/register">Register Now</Link>
+              </span>
+            </div>
           </div>
         </div>
       </div>
