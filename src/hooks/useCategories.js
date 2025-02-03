da711e149bcd73a037c280d2be37040045f207a9
devaanantawijaya <gededeva11@gmail.com>
2025-02-03 14:44:26 +0800

    menambahkan useCategories pada hooks

---
+++ src/hooks/useCategories.js
@@ -0,0 +1,28 @@
+import { API_KEY, BASE_URL } from "@/helper/endpoint";
+import axios from "axios";
+import { useState } from "react";
+
+const useCategories = () => {
+  const [categories, setCategories] = useState([]);
+  const [loadingCategories, setLoadingCategories] = useState(false);
+
+  const getCategories = async () => {
+    setLoadingCategories(true);
+    try {
+      const res = await axios.get(`${BASE_URL.API}/api/v1/categories`, {
+        headers: {
+          apiKey: API_KEY,
+        },
+      });
+      setCategories(res.data.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoadingCategories(false);
+    }
+  };
+
+  return { categories, loadingCategories, getCategories };
+};
+
+export default useCategories;
