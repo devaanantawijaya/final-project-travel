43b707508fe3ec7bfcff6e424998bcd55c0fb174
devaanantawijaya <gededeva11@gmail.com>
2025-02-03 14:42:12 +0800

    perbaikan bug pada card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,73 +3,62 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 
 const Card = (props) => {
   const { item, key, title, totalItems, onClick } = props;
-  // console.log(item)
+
   return (
     <div
       key={key}
-      className={`mx-4 ${totalItems < 3 && "sm:flex gap-x-5"} mb-5 sm:mb-0`}
+      className={`mx-4 ${totalItems === 2 && !item.price && "sm:flex gap-x-5"} mb-5 sm:mb-0`}
     >
       <div className="">
         <img
           src={
-            (item.imageUrl !== "" ? item.imageUrl : "/images/404.png") ||
-            (item.imageUrls[0] !== "" ? item.imageUrls[0] : "/images/404.png")
+            (item.imageUrl !== "" ? item.imageUrl : "/images/no-foto.jpg") ||
+            (item.imageUrls[0] !== ""
+              ? item.imageUrls[0]
+              : "/images/no-foto.jpg")
           }
-          alt={item.title}
+          alt={item.title || item.name}
           className="rounded-lg aspect-[16/9] w-full object-cover object-center h-40 border-2 border-orange-200"
           onError={(e) => {
-            e.target.src = "/images/404.png";
+            e.target.src = "/images/no-foto.jpg";
           }}
         />
       </div>
       <div
         className={`${
-          totalItems < 3 ? "sm:pt-0 pt-3 sm:w-52" : "pt-3"
+          totalItems === 2 && !item.price ? "sm:pt-0 pt-3 sm:w-52" : "pt-3"
         } flex flex-col sm:justify-between h-full`}
       >
         <div>
           <h1 className="font-bold">
-            {item.title.length > 30
-              ? item.title.slice(0, 30) + "..."
+            {item.title && item.title.length > 25
+              ? item.title.slice(0, 25) + "..."
               : item.title}
+            {item.name && item.name}
           </h1>
           <div className="flex gap-x-3">
-            {item.price && (
-              <p className="line-through text-gray-500">
-                {item.price.toLocaleString("id-ID", {
-                  style: "currency",
-                  currency: "IDR",
-                  minimumFractionDigits: 0,
-                  maximumFractionDigits: 0,
-                })}
-              </p>
-            )}
             {item.price_discount && (
-              <p>
-                {item.price_discount.toLocaleString("id-ID", {
-                  style: "currency",
-                  currency: "IDR",
-                  minimumFractionDigits: 0,
-                  maximumFractionDigits: 0,
-                })}
+              <p className="line-through text-gray-500">
+                Rp. {(item.price + item.price_discount).toLocaleString("id-ID")}
               </p>
             )}
+            {item.price && <p>Rp. {item.price.toLocaleString("id-ID")}</p>}
           </div>
-          {totalItems < 3 ? (
+          {totalItems === 2 && !item.price ? (
             <p>
-              {item.description.length > 40
+              {item.description && item.description.length > 40
                 ? item.description.slice(0, 40) + "..."
                 : item.description}
             </p>
           ) : (
             <p>
-              {item.description.length > 30
-                ? item.description.slice(0, 30) + "..."
+              {item.description && item.description.length > 25
+                ? item.description.slice(0, 25) + "..."
                 : item.description}
             </p>
           )}
         </div>
-        {totalItems < 3 ? (
+        {totalItems === 2 && !item.price ? (
           <Button
             title={
               <div className="flex items-center h-10">
