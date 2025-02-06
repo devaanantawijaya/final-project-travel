e8308cdeaf79f548624d2943de6420b192cbc403
devaanantawijaya <gededeva11@gmail.com>
2025-02-06 21:26:07 +0800

    perbaikan jalur admin dan user setelah login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -41,6 +41,9 @@ const LoginPage = () => {
             "bg-orange-500 hover:bg-orange-400 text-white font-bold py-2 px-4 rounded",
         },
       }).then(() => {
+        if (userData.role === "admin") {
+          router.push("/user");
+        }
         router.push("/");
       });
     } catch (error) {
