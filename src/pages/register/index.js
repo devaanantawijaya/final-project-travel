de8b53142bb2f55aacbe4a6bf08f53b47bfbfb9d
devaanantawijaya <gededeva11@gmail.com>
2025-02-06 21:30:46 +0800

    perbaikan jalur admin dan  user setelah register

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -90,6 +90,9 @@ const RegisterPage = () => {
             "bg-orange-500 hover:bg-orange-400 text-white font-bold py-2 px-4 rounded",
         },
       }).then(() => {
+        if (userData.role === "admin") {
+          router.push("/user");
+        }
         router.push("/");
       });
     } catch (error) {
