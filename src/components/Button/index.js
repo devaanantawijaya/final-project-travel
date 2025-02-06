83f1c69c2276e8095300525e3cc83298093560cf
devaanantawijaya <gededeva11@gmail.com>
2025-02-06 16:13:09 +0800

    tambah disable pada component button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,10 @@
 const Button = (props) => {
-  const { title, bg, p, text, onClick } = props;
+  const { title, bg, p, text, onClick, disabled } = props;
   return (
     <button
       className={`${bg} font-semibold ${p} rounded-2xl ${text}`}
       onClick={onClick}
+      disabled={disabled}
     >
       {title}
     </button>
