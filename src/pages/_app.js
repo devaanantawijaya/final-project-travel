1c1a7b75b30ded602244d81fa575ee7842049f87
devaanantawijaya <gededeva11@gmail.com>
2025-02-07 12:19:37 +0800

    tambahkan logo di tab website di _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,14 @@
 import { UserProvider } from "@/context/userContext";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
+      <Head>
+        <link rel="icon" type="image/png" href="/images/logo-travely.png" />
+        <title>Travely</title>
+      </Head>
       <Component {...pageProps} />
     </UserProvider>
   );
